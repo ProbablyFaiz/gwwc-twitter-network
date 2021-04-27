16dcb3ad8174bf1e995c1474a091180ede280ad6
Faiz Surani <faiz.surani@gmail.com>
2021-04-27 16:25:58 -0700

    Add top-n centrality calculation for scraped network data

--- neta/scraped-network-analysis.py
+++ neta/scraped-network-analysis.py
@@ -1,10 +1,11 @@
 import networkx as nx
 import csv
+import heapq
 
 EDGES_FILE_PATH = "../data/edges.csv"
 
-GWWC_NODES = [88534421, 363005534, 1062005076204642305, 116994659, 107336879, 519438862, 1183382935, 47268595, 37723353,
-              1110877798820777986, 181328570]
+GWWC_NODES = map(str, [88534421, 363005534, 1062005076204642305, 116994659, 107336879, 519438862, 1183382935, 47268595,
+                       37723353, 1110877798820777986, 181328570])
 
 
 def construct_graph():
@@ -17,5 +18,17 @@ def construct_graph():
     return follower_graph
 
 
+def top_n(collection, n):
+    heapq.heapify(collection)
+    top_n_items = []
+    while len(top_n_items) < n:
+        nth_lowest = heapq.heappop(collection)
+        top_n_items.append(nth_lowest)
+    return top_n_items
+
+
 graph = construct_graph()
-print(nx.number_of_edges(graph))
+centrality = [(-cent, node) for node, cent in nx.eigenvector_centrality_numpy(graph).items()]
+most_central = top_n(centrality, 20)
+print(most_central)
+
