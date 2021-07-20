cb3d1e21ad7fe2ff302739cfc678056b263fa51f
Tim Loderhose <tim@loderhose.com>
2021-07-20 23:43:34 +0200

    Add setup.py

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 from helpers import get_full_path
 
-EDGE_CSV_PATH = get_full_path('data/edges_following25.csv')
-NETWORK_CACHE_PATH = get_full_path('tmp/network_cache.pik')
+EDGE_CSV_PATH = get_full_path("data/edges_following25.csv")
+NETWORK_CACHE_PATH = get_full_path("tmp/network_cache.pik")
 USERS_FILE_PATH = get_full_path("data/users_following25.csv")
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="gwwc-twitter-network",
+    version="0.1",
+    author="Faiz Surani, Tim Loderhose",
+    description=("GWWC Twitter Network Analysis"),
+    long_description=readme,
+    url="https://github.com/ProbablyFaiz/gwwc-twitter-network",
+    license=None,
+    packages=find_packages(),
+)
